5bd2f7160a858c6e65006cd6d835b3727407e5de
Erica <ericaperry@quickenloans.com>
2021-01-06 14:49:21 -0500

    operator tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs';
+
+  ngOnInit() {
+    of(2, 4, 6, 7).subscribe(console.log);
+
+    from([1, 2, 3, 4, 5]).pipe(
+      tap(item => console.log(item)),
+      map(item => item - 3),
+      map(item => {
+        if (item >= 0) {
+          throw new Error('zero detected')
+        }
+        return item
+      }),
+      take(1)
+    ).subscribe(
+      item => console.log(item),
+      err => console.log(err),
+      () => console.log('complete')
+    );
+  }
 }
